c454303909ba3f82f0e89836cf3460cc116747ca
Jaya Mounica <mamidala.mounica@globallogic.com>
2025-02-11 15:07:33 +0530

    updated the api calls

--- Polestar-API/tests/get_api.spec.ts
+++ Polestar-API/tests/get_api.spec.ts
@@ -1,18 +1,46 @@
 const {test, expect} = require('@playwright/test')
+import{ faker } from '@faker-js/faker';
 
 test('Graph API POST Request', async({ request }) => {
-    const postAPIResponse = await request.get ('https://gorest.co.in/public/v2/users/7666399', {
+    const name = faker.person.fullName();
+    const email = faker.internet.email();
+    console.log(name,email)
+ 
+     const postAPIResponse = await request.post('https://gorest.co.in/public/v2/users', {
+        headers:{
+         "Authorization": "Bearer 66e1585618294e76a7386de64603bb6021fe54623ba1b8c3072da1ccc8d75bd4"
+        },
+       
+ 
+         data:{
+             "id": "",
+             "name": name,
+             "email": email,
+             "gender": "male",
+             "status": "active"
+         }
+     })
+ 
+    await  expect(postAPIResponse.ok()).toBeTruthy();
+    await  expect(postAPIResponse.status()).toBe(201);
+ 
+ 
+ 
+     const postAPIresponseBody = await postAPIResponse.json();
+     let userid = await postAPIresponseBody.id
+
+
+    const getAPIResponse = await request.get (`https://gorest.co.in/public/v2/users/${userid}`, {
        headers:{
         //"key": "Authorization",
         "Authorization": "Bearer 66e1585618294e76a7386de64603bb6021fe54623ba1b8c3072da1ccc8d75bd4"
        }
     })
 
-   await  expect(postAPIResponse.ok()).toBeTruthy();
-   await  expect(postAPIResponse.status()).toBe(200);
+   await  expect(await getAPIResponse.status()).toBe(200);
 
 
 
-    const postAPIresponseBody = await postAPIResponse.json();
-    console.log(postAPIresponseBody);
+    const getAPIresponseBody = await getAPIResponse.json();
+    console.log(getAPIresponseBody);
 })
--- Polestar-API/tests/put_api.spec.ts
+++ Polestar-API/tests/put_api.spec.ts
@@ -1,26 +1,49 @@
 const {test, expect} = require('@playwright/test')
+import{ faker } from '@faker-js/faker';
 
 test('Graph API PUT Request', async({ request }) => {
-    const postAPIResponse = await request.put('https://gorest.co.in/public/v2/users/7666399', {
+    const name = faker.person.fullName();
+    const email = faker.internet.email();
+    console.log(name,email)
+ 
+     const postAPIResponse = await request.post('https://gorest.co.in/public/v2/users', {
+        headers:{
+         "Authorization": "Bearer 66e1585618294e76a7386de64603bb6021fe54623ba1b8c3072da1ccc8d75bd4"
+        },
+       
+ 
+         data:{
+             "id": "",
+             "name": name,
+             "email": email,
+             "gender": "male",
+             "status": "active"
+         }
+     })
+ 
+     const postAPIresponseBody = await postAPIResponse.json();
+     const userid = postAPIresponseBody.id;
+     console.log(postAPIresponseBody)
+
+    const putAPIResponse = await request.put(`https://gorest.co.in/public/v2/users/${userid}`, {
        headers:{
         "Authorization": "Bearer 66e1585618294e76a7386de64603bb6021fe54623ba1b8c3072da1ccc8d75bd4"
        },
-      
-       
+
         data:{
-            "id": "7653259",
-            "name": "Test QA012",
-            "email": "test@test.com012",
-            "gender": "male",
+            "id": userid,
+            "name": name,
+            "email": email,
+            "gender": "female",
             "status": "active"
         }
     })
 
    await  expect(postAPIResponse.ok()).toBeTruthy();
-   await  expect(postAPIResponse.status()).toBe(200);
+   await  expect(putAPIResponse.status()).toBe(200);
 
 
 
-    const postAPIresponseBody = await postAPIResponse.json();
-    console.log(postAPIresponseBody);
+    const putAPIresponseBody = await putAPIResponse.json();
+    console.log(putAPIresponseBody);
 })
