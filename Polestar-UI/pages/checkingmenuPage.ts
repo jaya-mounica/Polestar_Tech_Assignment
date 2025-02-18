fb87dea47e42ab261ee89dfd849ba751c412180c
Jaya Mounica <mamidala.mounica@globallogic.com>
2025-02-18 10:37:59 +0530

    Fix: updated code format

--- Polestar-UI/pages/checkingmenuPage.ts
+++ Polestar-UI/pages/checkingmenuPage.ts
@@ -7,7 +7,6 @@ import { locators } from "../Locators/HomepageElements";
 
 class checkingmenuPage extends playwrightWrapper {
     locator: locators;
-
     polestar3Menu: any
     HoveronTestDrive: any
     TestDriveText: any
@@ -16,10 +15,6 @@ class checkingmenuPage extends playwrightWrapper {
         super(pagefixture.page)
         this.page = page;
         this.locator = new locators();
-        // this.polestar3Menu =this.page.locator("(//div[@class='css-1jof5mm']/button)[2]")
-        // this.HoveronTestDrive =this.page.locator("//a[@id='RBw8ZjGeRJ2y4-BDFbAUJw']")
-        // this.TestDriveText = this.page.locator("(//div[@class='css-289fc1']//h1)[1]");
-
     }
 
     async verifyPolestar3MenuOption() {
--- Polestar-UI/pages/loginPage.ts
+++ Polestar-UI/pages/loginPage.ts
@@ -6,14 +6,11 @@ import { setDefaultTimeout } from "@cucumber/cucumber";
 import { login, } from "../helper/login.enum";
 //import * as data from "../helper/login.json"
 import { locators } from "../Locators/LoginPageElements";
-
-
 setDefaultTimeout(60 * 1000)
 
 
 class loginPage extends playwrightWrapper {
   locator: locators;
-
   acceptCookies: any
   profileMenu: any
   email: any
@@ -25,12 +22,6 @@ class loginPage extends playwrightWrapper {
     super(pagefixture.page)
     this.page = page;
     this.locator = new locators();
-    // //this.acceptCookies = pagefixture.page.getByRole('button', { name: 'Accept all' })
-    // this.profileMenu = this.page.locator("//a[@id='L_aKvtA0TE-V7FLPcm1OLQ']/div/span[@class='css-62qso3']")
-    // this.email = this.page.locator("//input[@id='email-username-field']")
-    // this.password = this.page.locator("//input[@id='password-field']")
-    // this.loginbutton = this.page.locator("//button[@id='login-btn']")
-    // this.errorValidation = this.page.locator("//div[@id='errorSection']")
   }
 
 
@@ -43,23 +34,17 @@ class loginPage extends playwrightWrapper {
   }
 
   async inputemailid() {
-    console.log(login.email)
     await this.page.locator(this.locator.email.xpath).fill(login.email)
-    //await this.fill(this.locator.email.xpath);
-
     // this.email.fill(data.email);
   }
 
   async inputPassword() {
     await this.page.locator(this.locator.password.xpath).fill(login.password)
-
-    //this.password.fill(login.password)
     //this.password.fill(data.password);
   }
 
   async Verifyloginbutton() {
     await this.page.locator(this.locator.loginbutton.xpath).click();
-    // await this.page.pause();
   }
 
   async VerifyerrorValidation() {
